94bc2b128efb771a9bf0121cb902873580be0c88
fionto <251855098+fionto@users.noreply.github.com>
2026-06-01 10:15:39 +0200

    feat: initial project skeleton with directory structure

---
+++ src/data_refinery/__init__.py
@@ -0,0 +1,8 @@
+"""
+data-refinery: A modular, observable data-ingress validation pipeline
+for research lab files.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Matteo"
+__description__ = "Fail-fast validation pipeline for research characterization data"
